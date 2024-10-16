d3089fd3ae16028fbfb73f0525541705bcca0422
Archana <archooworship@gmail.com>
2024-10-15 20:16:55 -0700

    committed

--- src/eightpattern/fullboundary.java
+++ src/eightpattern/fullboundary.java
@@ -235,7 +235,7 @@ public static void halfPyramid() {
 		
 		for(int i = 1; i <= n; i++) {
 			
-			for (int j =1 ;  j <=n-i ; j++) {
+			for (int j =1 ;  j <=n+i ; j++) {
 				
 				System.out.print(" ");
 				
